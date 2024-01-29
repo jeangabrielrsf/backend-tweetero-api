c079823b05fa83e7129ad291c55f3a586900b18a
Jean Frusca <jeang96@gmail.com>
2024-01-29 16:20:28 -0300

    [fix] corrigidas entidades de usuários e tweets

--- src/main/java/com/tweetero/tweetero/models/TweetModel.java
+++ src/main/java/com/tweetero/tweetero/models/TweetModel.java
@@ -5,6 +5,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -22,4 +24,8 @@ public class TweetModel {
 
     @Column(length = 280, nullable = false)
     private String text;
+
+    @ManyToOne
+    @JoinColumn(name = "users_id")
+    private UserModel userModel;
 }
